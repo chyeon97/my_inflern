1abc33ac2f3a093438fdcddef0da866d932c6385
chyeon97 <pcyeon07@gmail.com>
2025-04-21 22:56:46 +0900

    한입북스 UI

---
+++ section02/src/components/searchable-layout.tsx
@@ -0,0 +1,38 @@
+import style from './searchable-layout.module.css';
+import { ReactNode, useEffect, useState } from "react"
+import {useRouter} from 'next/router'
+
+export default function SearchableLayout({children}: {children: ReactNode}) {
+    const router = useRouter();
+    const [search, setSearch] = useState("");
+    const q = router.query.q as string;
+
+    useEffect(() => {
+        setSearch(q || "")
+    }, [q])
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
+    const onSubmit = () => {
+        if(!search || q === search) return;
+        router.push(`/search?q=${search}`)
+    }
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Enter"){
+            onSubmit();
+        }
+    }
+
+    return (
+        <div>
+            <div className={style.searchbar_container}>
+                <input onKeyDown={onKeyDown}  placeholder="검색어를 입력하세요..." onChange={onChange}/>
+                <button onClick={onSubmit}>검색</button>
+            </div>
+            {children}
+        </div>
+    )
+}
---
+++ section02/src/pages/search/index.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from "react";
+import SearchableLayout from "@/components/searchable-layout";
+import books from '@/mock/books.json'
+import BookItem from "@/components/book-item";
+
+export default function Search() {
+    return (
+      <div>
+        {books.map((book) => <BookItem key={book.id} {...book}/>)}
+      </div>
+    );
+  }
+  
+  Search.getLayout = (page: ReactNode) => {
+    return <SearchableLayout>{page}</SearchableLayout>
+  }
